1af8482f63c155aa254d1332d1544a00792bdbb7
Alex <palaciosalex07@gmail.com>
2023-07-17 19:14:03 -0500

    cambio

---
+++ script.js
@@ -0,0 +1,12 @@
+var textos=[
+ "Para las tablas se utilizo el plugin DataTables",
+ "",
+ "Se utilizo el plugin SweetAlert para mostrar mensaje",
+ "El sistema permite importa las asistencias mediante un archivo Excell. Usando el paquete Carbon",
+]
+
+
+function cargarfoto(img, txt,nproy){
+document.getElementById("galeria"+nproy).src="img/proyecto"+nproy+"/img"+img+".PNG";
+document.getElementById("mitexto").innerHTML=textos[txt];
+}
